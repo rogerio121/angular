c5d2690f114c36b4069952a6f8b802c98635719e
rogerio121 <rogeriodominus@gmail.com>
2019-12-20 23:16:54 -0300

    Introdução Typescript

---
+++ hello-typescript/main.js
@@ -0,0 +1,17 @@
+var minhaVariavel = 'minha variável';
+function minhaFuncao(x, y) {
+    return x + y;
+}
+var num = 2;
+var pi = 3.14;
+var numeros = [1, 2, 3];
+numeros.map(function (valor) { return valor * 2; });
+var Matematica = /** @class */ (function () {
+    function Matematica() {
+    }
+    Matematica.prototype.soma = function (x, y) {
+        return x + y;
+    };
+    return Matematica;
+}());
+var n1 = 'Texto';
---
+++ hello-typescript/main.ts
@@ -0,0 +1,21 @@
+var minhaVariavel = 'minha variável';
+
+function minhaFuncao(x, y){
+    return x + y;
+}
+
+let num = 2;
+
+const pi = 3.14;
+
+var numeros = [1, 2, 3];
+
+numeros.map( valor => valor * 2);
+
+class Matematica{
+    soma(x, y){
+        return x + y;
+    }
+}
+
+var n1: string = 'Texto';
