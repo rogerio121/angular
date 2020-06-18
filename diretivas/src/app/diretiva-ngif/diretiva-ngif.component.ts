ab504508869f09fe99f0fe7d645728fbe579bfab
rogerio <rogeriodominus@gmail.com>
2020-06-18 19:22:23 -0300

    Diretiva - ngIf

--- diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -3,11 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-diretiva-ngif',
   templateUrl: './diretiva-ngif.component.html',
-  styleUrls: ['./diretiva-ngif.component.css']
+  styleUrls: ['./diretiva-ngif.component.scss']
 })
 export class DiretivaNgifComponent implements OnInit {
 
+  cursos: string[] = [];
+  mostrar: boolean = true;
   constructor() { }
+
+  onMostrarOuEsconder(){
+    this.mostrar = !this.mostrar;
+  }
 
   ngOnInit(): void {
   }
