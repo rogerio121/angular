ded14d2a1c57644ffcd2a3ca477a5a03f9a72dee
rogerio <rogeriodominus@gmail.com>
2020-06-17 22:29:56 -0300

    Usando libs externas (Bootstrap, Materialize, JQuery e Lodash)

--- angular-cli-libs-externas/src/app/app.component.ts
+++ angular-cli-libs-externas/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import * as M from "materialize-css/dist/js/materialize";
+import * as _ from "lodash";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'angular-cli-libs-externas';
+  title = 'angular-cli-libs-externas';      
+
+  
+  ngAfterViewInit() {
+    
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.collapsible');
+      var instances = M.Collapsible.init(elems, {});
+    });
+  }
+
+  //lodash
+  list = _.map([1,2,3], (n) => `# ${n}`)
 }
