24cff188f6c05bfaebefc1305c58d0fdd03e205c
macpro <macpro@MacBook-Pro-de-macpro.local>
2020-08-07 22:59:24 -0700

    wip: Ádd curso

--- servicos/src/app/cursos/cursos.service.ts
+++ servicos/src/app/cursos/cursos.service.ts
@@ -3,7 +3,13 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class CursosService{
 
+    private cursos : string[] = ['Angular', 'Java'];
+
     getCursos(){
-        return ['Angular', 'Java'];
+        return this.cursos;
+    }
+
+    addCurso(cursos: string){
+        this.cursos.push(cursos);
     }
 }
