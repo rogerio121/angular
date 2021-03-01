87eed931731c0730c4f9a81cdfc0ef02100e8c40
rogedam <rogeriodominus@gmail.com>
2021-02-28 21:29:15 -0300

    Rotas: Passando id para rota 1

--- rotas/src/app/app-routing.module.ts
+++ rotas/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CursosComponent } from './cursos/cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'cursos', component: CursosComponent}
+  {path: 'cursos', component: CursosComponent},
+  {path: 'curso/:id', component: CursoDetalheComponent}
 ];
 
 @NgModule({
--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { HeaderComponent } from './header/header.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HeaderComponent } from './header/header.component';
     HomeComponent,
     LoginComponent,
     CursosComponent,
-    HeaderComponent
+    HeaderComponent,
+    CursoDetalheComponent
   ],
   imports: [
     BrowserModule,
